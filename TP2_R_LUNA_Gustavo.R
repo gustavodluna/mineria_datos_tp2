b0c407bc30943590f1a19027619e3cb1e4add5ae
Gustavo <gustavodavid097@gmail.com>
2024-09-06 15:13:51 -0300

    commit 2

---
+++ TP2_R_LUNA_Gustavo.R
@@ -0,0 +1,7 @@
+
+library(readr)
+
+detroit-> readr::read_csv(file="detroit.csv")
+
+asociaciones <- readr::read_csv(file =
+                                  "https://raw.githubusercontent.com/gastonbecerra/curso-intro-r/main/data/asociaciones.csv")
