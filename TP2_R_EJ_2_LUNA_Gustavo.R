b73234bd7e52bfbc1f53412f436fa966ca9dadf3
Gustavo <gustavodavid097@gmail.com>
2024-09-07 14:11:47 -0300

    commit 13

--- TP2_R_EJ_2_LUNA_Gustavo.R
+++ TP2_R_EJ_2_LUNA_Gustavo.R
@@ -137,3 +137,140 @@ for (k in 2:4){
     qqline(x)
   }
 }
+
+
+# Contraste de normalidad Shapiro-Wilk para cada variable en cada especie
+
+library(reshape2)
+library(knitr)
+library(dplyr)
+
+datos_tidy <- melt(datos, value.name = "valor")
+
+summary_table <- datos_tidy %>% 
+  group_by(especie, variable) %>% 
+  summarise(p_value_Shapiro.test = shapiro.test(valor)$p.value)
+
+# Mostrar el resultado
+kable(summary_table)
+
+## |especie |variable      | p_value_Shapiro.test|
+## |:-------|:-------------|--------------------:|
+## |a       |pata          |            0.7763034|
+## |a       |abdomen       |            0.1845349|
+## |a       |organo_sexual |            0.6430844|
+## |b       |pata          |            0.7985711|
+## |b       |abdomen       |            0.5538213|
+## |b       |organo_sexual |            0.8217855|
+
+# No hay evidencias de falta de normalidad univariante en ninguna de las variables 
+#empleadas como predictores en ninguno de los grupos.
+
+# El paquete MVN contiene funciones que permiten realizar los tres test de hipótesis
+#comúnmente empleados para evaluar la normalidad multivariante (Mardia, Henze-Zirkler 
+#y Royston) y también funciones para identificar outliers que puedan influenciar 
+#en el contraste.
+
+library(MVN)
+
+par(mar = c(1, 1, 1, 1))
+outliers <- mvn(data = datos[,-1], mvnTest = "hz", multivariateOutlierMethod = "quan")
+
+
+royston_test<-mvn(data=datos[,-1],mvnTest = "royston",multivariatePlot="qq")
+
+
+royston_test$multivariateNormality
+
+##Test         H     p value   MVN
+##Royston  0.4636176 0.9299447 YES
+
+hz_test<-mvn(data=datos[,-1],mvnTest = "hz")
+hz_test$multivariateNormality
+
+##       Test        HZ    p value    MVN
+##Henze-Zirkler  0.7870498 0.07666139 YES
+
+
+# A pesar de los 6 outliers detectados, ninguno de los dos test encuentran evidencias
+#significativas (α=0.05 ) de falta de normalidad multivariante.
+
+# Finalmente, mediante la función boxM() del paquete biotools se realiza el contraste de
+#matrices de covarianza.
+
+
+#remotes::install_version("biotools", version = "3.1") 
+packageVersion("biotools")
+#install.packages("D:/Descargas/biotools_3.1.tar.gz", repos = NULL, type = "source")
+library(biotools)
+
+boxM(data=datos[,2:4],grouping=datos[,1])
+
+
+##        Box's M-test for Homogeneity of Covariance Matrices
+##
+## data:  datos[, 2:4]
+## Chi-Sq (approx.) = 9.831, df = 6, p-value = 0.132
+
+
+# Se puede aceptar que la matriz de covarianza es igual en todos los grupos.
+#Estimación de los parámetros de la función de densidad (μ^(X),∑) y cálculo de la función
+#discriminante.
+#Estos dos pasos se realizan mediante la función lda del paquete MASS. lda realiza la
+#clasificación mediante la aproximación de Fisher.
+
+
+modelo_lda <-lda(formula=especie~pata+abdomen+organo_sexual,data=datos)
+
+# Una vez obtenidas las funciones discriminantes, se puede clasificar un nuevo insecto en
+#función de sus medidas. Por ejemplo, un nuevo espécimen cuyas medidas sean: pata = 194,
+#abdomen = 124, organo_sexual = 49.
+
+
+nuevas_observaciones<-data.frame(
+                                pata=194,
+                                abdomen=124,
+                                organo_sexual=49
+                                 )
+predict(object = modelo_lda,newdata = nuevas_observaciones)
+
+##$class
+##[1] b
+##Levels: a b
+
+##$posterior
+##a         b
+##1 0.05823333 0.9417667
+
+##$x
+##LD1
+##1 0.5419421
+
+
+# El resultado muestra que, según la función discriminante, la probabilidad posterior 
+#de que el espécimen pertenezca a la especie b es del 94.2% frente al 5.8% de que 
+#pertenezca a la especie a.
+
+# Evaluación de los errores de clasificación.
+
+predicciones<-predict(
+                      object = modelo_lda,
+                      newdata = datos[,-1],
+                      method = "predictive"
+                      )
+table(datos$especie,
+      predicciones$class,
+      dnn=c("Clase real","Clase predicha")
+      )
+
+##           Clase predicha
+##Clase real  a  b
+##         a 10  0
+##         b  0 10
+
+training_error<-mean(datos$especie!=predicciones$class)*100
+
+paste("training_error=",training_error,"%")
+##[1] "training_error= 0 %"]
+
+
